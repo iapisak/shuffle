092b15eb80a2b3ac955dbc891c89847d34f96621
iapisak <apotisanmetee@gmail.com>
2021-04-12 03:24:07 -0700

    edit styling

--- src/Components/Nav/navBar.js
+++ src/Components/Nav/navBar.js
@@ -7,22 +7,22 @@ export default function NavBar ({ searchKey, setSearchKey, recentlyPlayed, toggl
 
     return  <nav className="container navbar navbar-expand-md navbar-dark px-0 px-3"
                  style={{ backgroundColor: '#5d4954' }}>
-                <h4 className="m-0 text-light"
+                <h2 className="m-0 py-1"
                     style={{ textShadow: '0 0.05rem 0.1rem rgba(0,0,0,0.2))', cursor: 'pointer' }} 
-                    onClick={()=> setSearchKey('') }>Shuffle by Spotify</h4>
-                <button className="navbar-toggler collapsed my-3 text-dark" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    onClick={()=> setSearchKey('') }>Shuffle by Spotify</h2>
+                <button className="navbar-toggler collapsed my-3 text-dark" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false">
                     <span className="navbar-toggler-icon text-dark"></span>
                 </button>
                 <div className="navbar-collapse collapse" id="navbarCollapse">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item mr-md-2">
-                            <input className="form-control mr-sm-2 border-0 bg-light" 
+                        <li className="nav-item">
+                            <input className="form-control border-0 p-2" 
                                 style={{ borderRadius: '30px' }}
-                                type="search" placeholder="Search songs/Artists" aria-label="Search" 
+                                type="search" placeholder="Search Songs/Artists" aria-label="Search" 
                                 value={ searchKey }
                                 onChange={(e)=> setSearchKey(e.target.value)} />
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                             <div className="nav-link navbar-link" 
                                  onClick={()=> { 
                                      setToggle(false)
@@ -36,7 +36,7 @@ export default function NavBar ({ searchKey, setSearchKey, recentlyPlayed, toggl
                                                             setSearchKey('')
                                                         } }>Recently Played</div>
                                                     </li> : null }
-                        <li className="nav-item">
+                        <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                             <a className="nav-link" 
                                 href='/'
                                 onClick={()=> logOut() }>Log out</a>
